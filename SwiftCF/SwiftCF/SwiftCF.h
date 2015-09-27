27ff766509ee7cc74d25a09cfc047250c24d1bdc
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-27 18:57:41 -0500

    created project

---
+++ SwiftCF/SwiftCF/SwiftCF.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCF.h
+//  SwiftCF
+//
+//  Created by Alsey Coleman Miller on 9/27/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCF.
+FOUNDATION_EXPORT double SwiftCFVersionNumber;
+
+//! Project version string for SwiftCF.
+FOUNDATION_EXPORT const unsigned char SwiftCFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCF/PublicHeader.h>
+
+
